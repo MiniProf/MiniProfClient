0a6376671c843880d642effe23b8c66fb2bddba1
George Wilson <g.wilson@lancsater.ac.uk>
2016-12-06 01:08:51 +0000

    removed things to make it run on android But non of Mo's Things !

--- www/js/index.js
+++ www/js/index.js
@@ -4,23 +4,6 @@ var app ={
   },
   onDeviceReady: function() {
 
-  },
-  prefLSID:"Prefs",
-  bookLSID:"BookPos",
-  getPreferences:()=>{
-    return JSON.parse(window.localStorage.getItem(this.prefLSID));
-  },
-  setPreferences:(items)=>{
-    window.localStorage.setItem(this.prefLSID,JSON.stringify(items));
-  },
-  setBookPos:(pos)=>{
-    window.localStorage.setItem(this.bookLSID,pos);
-  },
-  getBookPos:()=>{
-    return window.localStorage.getItem(this.bookLSID);
-  },
-  removeBookPos:()=>{
-    window.localStorage.setItem(this.bookLSID,"");
   }
 };
 
--- www/react/components.js
+++ www/react/components.js
@@ -1,7 +1,6 @@
 var Screen = require('./Screen.jsx');
 var React  = require('react');
 var ReactDOM  = require('react-dom');
-var {} = require("elemental");
 //console.log(React);
 $(document).ready(      function(){
        ReactDOM.render(React.createElement(Screen,{}),document.body);
